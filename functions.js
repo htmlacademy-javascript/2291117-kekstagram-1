3c28e4f3a4103324019a65cd56d6c24b58b3a7ed
gleb-mezenin <gleb-mezenin@mail.ru>
2023-09-03 22:07:44 +0500

    Исправляет замечания по домашке

--- functions.js
+++ functions.js
@@ -1,62 +1,58 @@
 const isPalindrom = (string) => {
   const tempString = string.toLowerCase().replaceAll(' ', '');
-  let palindomString = '';
-  for (let i = tempString.length - 1; i >= 0; i--) {
-    palindomString += tempString.at(i);
-  }
-  console.log(palindomString);
-  if (palindomString === tempString) {
-    console.log('Это палиндром');
+  if (tempString.at(0) !== tempString.at(-1)) {
+    return false;
   } else {
-    console.log('Это не палиндром');
+    let palindomString = '';
+    for (let i = tempString.length - 1; i >= 0; i--) {
+      palindomString += tempString.at(i);
+    }
+    if (palindomString === tempString) {
+      return true;
+    }
   }
 };
 isPalindrom('Лёша на полке клопа нашёл ');
 
-const isCheckStringLength = (string, length) => {
+
+const isCompareLength = (string, length) => {
   if (string.length - 1 < length) {
-    console.log(true);
-  } else {
-    console.log(false);
+    return true;
   }
 };
-isCheckStringLength('проверяемая строка', 5);
+isCompareLength('проверяемая строка', 5);
 
-const convertNumber = (string) => {
-  const tempString = string.replaceAll(' ', '');
+const convertToNumber = (string) => {
   let newNumber = '';
-  for (let i = 0; i < tempString.length; i++) {
-    if (!isNaN(tempString.at(i))) {
-      newNumber += tempString.at(i);
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      newNumber += string.at(i);
     }
   }
-  const tempNumber = parseInt(newNumber, 10);
-  console.log(tempNumber);
+  return parseInt(newNumber, 10);
 };
-convertNumber('-1');
+convertToNumber('-1');
+
+/*
+1 - сравнить длину исходной строки с результатом, если исходная больше или равна, вернуть строку
+2 - иначе, нужно узнать, сколько у нас свободных символов, тут простая математика, отнимем от ожидаемой
+длинны длину исходной строки ( как вариант, ты можешь сделать это в первом пункте, и сравнивать не длинны, а число )
+3 - дальше мы должны узнать сколько целых частей добавочной строки мы можем добавить, тут можно поделить то что
+у нас с пункта 2 на длину строки без остатка, и создать нужную строку через string repeat
+*/
 
 const makeAddress = (originalString, minLength, additionalString) => {
-  let tempString = '';
-  let newString = '';
-  /*if (additionalString.length <= minLength - originalString.length) {
-    for (let i = 0; i < minLength - originalString.length; i++) {
-      /*newString = additionalString.repeat(minLength - originalString.length) + originalString;
-      tempString += additionalString.at();
-    }
-  } else {
-    for (let i = 0; i < minLength - originalString.length; i++) {
-      tempString += additionalString.repeat();
-    }
-  }*/
-  if(originalString.length + additionalString.length < minLength) {
-    tempString = additionalString.repeat(minLength - originalString.length) + originalString;
-    console.log(tempString);
+  const numberOfSumbols = minLength - originalString.length;
+  if (numberOfSumbols <= 0) {
+    return originalString;
   }
-  /*newString = tempString + originalString;
-  console.log(newString);*/
+  const tempString = additionalString.slice(0, numberOfSumbols % additionalString.length);
+  const repeatString = additionalString.repeat(numberOfSumbols / additionalString.length);
+  return tempString + repeatString + originalString;
 };
+
 makeAddress('1', 2, '0'); // '01'
 makeAddress('1', 4, '0'); // '0001'
 makeAddress('q', 4, 'werty'); // 'werq'
 makeAddress('q', 4, 'we'); // 'wweq'
-/*makeAddress('qwerty', 4, '0'); // 'qwerty'*/
+makeAddress('qwerty', 4, '0'); // 'qwerty'
